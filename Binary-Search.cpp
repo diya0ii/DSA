b736debb8735dbdfa0d5c97e5d1ce35feba519c7
Diya Verma <76639881+diya0ii@users.noreply.github.com>
2025-08-06 11:35:46 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 79.73%] - LeetPush

--- Binary-Search.cpp
+++ Binary-Search.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int i=0;
-        int index;
-        int n=nums.size();
-        int j=n-1;
-        while(i<=j){
-            int k=i+(j-i)/2;
-            if(nums[k]==target){
-                return k;
+        int n= nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            else if(nums[k]<target){
-                i=k+1;
+            else if(nums[mid]<target){
+                low=mid+1;
             }
-            else {
-                j=k-1;
+            else{
+                high= mid-1;
             }
-       
         }
         return -1;
     }
