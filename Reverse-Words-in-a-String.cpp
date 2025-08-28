3f52dd8c617530a4cd59492cebc03d5e38e5704a
Diya Verma <76639881+diya0ii@users.noreply.github.com>
2025-08-28 09:38:43 +0530

    [151] [Time Beats: 100.00%] [Memory Beats: 98.51%] - LeetPush

---
+++ Reverse-Words-in-a-String.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n= s.size();
+        reverse(s.begin(),s.end());
+        int i=0,left=0,right=0;
+        while(i<n){
+            while(i<n && s[i]==' '){
+                i++;
+            }
+            if(i==n) break;
+            while(i<n && s[i]!=' '){
+                s[right++]=s[i++];
+            }
+            reverse(s.begin() +left, s.begin() +right);
+            s[right++]=' ';
+            left= right;
+            i++;
+        }
+        s.resize(right-1);
+        return s;
+    }
+};
